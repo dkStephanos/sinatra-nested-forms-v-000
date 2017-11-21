4ec4a675377f612d7efff59c3a671d648147f2d0
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-21 17:59:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ module FormsLab
     end
 
     post "/pirates" do
-      
+
       @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |details|
@@ -20,7 +20,7 @@ module FormsLab
       end
 
       @ships = Ship.all
-      
+
       erb :"pirates/show"
     end
 
